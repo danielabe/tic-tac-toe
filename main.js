692abbc552b44b6792b8a63936eae136c1eb8095
Daniela Martina Berardi Dichiara <danielaberardi@live.com.ar>
2021-06-13 20:31:17 -0300

    change turns

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const button = document.getElementById('button')
 const cells = document.querySelectorAll('.cell')
 
+let icon = '<i class="fas fa-rocket"></i>'
+let turn = 'first'
+
 button.addEventListener('click', () => startGame())
 
 function startGame() {
@@ -9,4 +12,20 @@ function startGame() {
 
 console.log(cells)
 
-cells.forEach(cell => cell.addEventListener('click', () => console.log(cell.id)))
+cells.forEach(cell => cell.addEventListener('click', () => {
+    if(cell.innerHTML === '') {
+        turnFunction()
+        cell.innerHTML = icon
+    }
+    console.log(turn + ' es tu turno')
+}))
+
+function turnFunction() {
+    if(turn === 'first') {
+        icon = '<i class="fas fa-rocket"></i>'
+        turn = 'second'
+    } else if (turn === 'second') {
+        icon = '<i class="fas fa-meteor"></i>'
+        turn = 'first'
+    }
+}
